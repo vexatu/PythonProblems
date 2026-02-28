3d5dec0011dd25b8f0c92b6602cad0ced38e4294
David Jipa <126858419+vexatu@users.noreply.github.com>
2026-02-28 20:22:20 +0100

    Update P3.py to process pairs of numbers
    Refactor input handling to allow multiple pairs and validate length.

--- P3.py
+++ P3.py
@@ -1,4 +1,29 @@
-liste = int(input())
-for i in range(1,liste+1):
-    numbers = input().split()
-    print(int(numbers[0]) + int(numbers[1]),"\n")
+length = int(input("Introduce the number of sets: "))
+
+while True:
+    list_of_numbers = []
+    for i in range(length):
+        line = input(f"Enter pair #{i + 1} separated by space: ")
+        nums = line.split()
+        list_of_numbers.extend(int(n) for n in nums)
+    if len(list_of_numbers) != length * 2:
+        print("Length does not match")
+        continue
+    break
+
+
+
+even = []
+odd = []
+for i in range(len(list_of_numbers)):
+    if i % 2 == 0:
+        even.append(list_of_numbers[i])
+    else:
+        odd.append(list_of_numbers[i])
+
+list_of_sums = []
+for i,j in zip(even, odd):
+    suma = i + j
+    list_of_sums.append(suma)
+
+print(list_of_sums)
