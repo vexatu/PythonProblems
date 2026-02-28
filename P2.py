6bae38689eabde27530596ce2997172917de055d
David Jipa <126858419+vexatu@users.noreply.github.com>
2026-02-28 20:20:55 +0100

    Refactor list input and total calculation
    Refactored code to use sum() for total calculation and improved input prompts.

--- P2.py
+++ P2.py
@@ -1,8 +1,9 @@
-n = int(input("Introduceti marimea listei:"))
-element = input("Introduceti elementele listei: ")
-my_list = list(map(int, element.split()))
-print(my_list)
-total = 0
-for num in my_list:
-    total += num
+length = int(input("Input how many numbers: "))
+numbers = input("Input numbers with space between them: ")
+A = []
+
+for num in numbers.split(): #iterates in the numbers variables and splits the string inserted
+    A.append(int(num)) #adds the split strings to the empty list and transform each in integer values
+
+total = sum(A)
 print(total)
