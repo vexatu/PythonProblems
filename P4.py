f7c81dc22719a4818e508fa81698636b45c981c6
David Jipa <126858419+vexatu@users.noreply.github.com>
2026-02-28 20:18:43 +0100

    modify

--- P4.py
+++ P4.py
@@ -1,9 +1,4 @@
-liste = int(input("Nr de perechi: "))
-new_list = []
-for i in range(1, liste+1):
-    numbers = input().split()
-    new_list.append(str(min(int(numbers[0]), int(numbers[1]))))
-print(" ".join(new_list))
+
 
 
 
