0d9621858c3aaf9c7e3e220258f3c235e4541336
David Jipa <126858419+vexatu@users.noreply.github.com>
2026-03-01 10:56:44 +0100

    Refactor input handling in P2.py

--- Week 1/P2.py
+++ Week 1/P2.py
@@ -2,8 +2,9 @@ length = int(input("Input how many numbers: "))
 numbers = input("Input numbers with space between them: ")
 A = []
 
-for num in numbers.split(): #iterates in the numbers variables and splits the string inserted
+for num in numbers.split(): #iterates in the numbers list and splits the string inserted
     A.append(int(num)) #adds the split strings to the empty list and transform each in integer values
 
 total = sum(A)
 print(total)
+
