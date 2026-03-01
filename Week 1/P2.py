91b217c20a31866752c05d5198c4caeec6a1eb37
David Jipa <126858419+vexatu@users.noreply.github.com>
2026-03-01 10:57:11 +0100

    Fix formatting and improve comments in P2.py

--- Week 1/P2.py
+++ Week 1/P2.py
@@ -2,9 +2,10 @@ length = int(input("Input how many numbers: "))
 numbers = input("Input numbers with space between them: ")
 A = []
 
-for num in numbers.split(): #iterates in the numbers list and splits the string inserted
+for num in numbers.split(): #iterates in the numbers list of strings and splits the strings inserted
     A.append(int(num)) #adds the split strings to the empty list and transform each in integer values
 
 total = sum(A)
 print(total)
+
 
